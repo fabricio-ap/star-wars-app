b869b9896473a8663d5ec7ae545b85033ea9c3cf
Fabricio Araujo Pereira <fabricioaraujo051@gmail.com>
2022-03-03 11:54:59 -0300

    Adding expo fonts

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
+import {
+  SourceSansPro_400Regular,
+  SourceSansPro_600SemiBold,
+  SourceSansPro_700Bold,
+  SourceSansPro_900Black,
+  useFonts,
+} from '@expo-google-fonts/source-sans-pro';
+import AppLoading from 'expo-app-loading';
 import SplashScreen from './src/screens/SplashScreen';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    SourceSansPro_400Regular,
+    SourceSansPro_600SemiBold,
+    SourceSansPro_700Bold,
+    SourceSansPro_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <SplashScreen />;
 }
-
-// https://www.youtube.com/watch?v=a2e2lXC5EGM&list=PL4zG19BCs4pdPJzElbUxCykHTClU-B0Ts&index=1
--- src/components/atoms/Text/styles.js
+++ src/components/atoms/Text/styles.js
@@ -3,8 +3,8 @@ import styled from 'styled-components/native';
 export const CustomText = styled.Text`
   font-size: 24px;
   font-style: normal;
-  font-weight: bold;
   line-height: 30px;
+  font-family: 'SourceSansPro_700Bold';
   color: ${({ color }) => color || 'white'};
   margin-top: 16px;
 `;
